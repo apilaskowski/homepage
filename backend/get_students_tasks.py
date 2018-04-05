f34bf446dc6bb9a32c174d4eb2619a8d4671387f
Artur Laskowski <artur.pi.laskowski@gmail.com>
2018-04-05 14:20:21 +0200

    FIX: Cleanup.

--- backend/get_students_tasks.py
+++ backend/get_students_tasks.py
@@ -3,19 +3,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
 from bs4 import BeautifulSoup
-
-driver = webdriver.Firefox()
 
 students = {}
 
 contest = "https://www.hackerrank.com/contests/ap-0"
 leaderboard = "/leaderboard/"
 
-contest_link = "https://www.hackerrank.com/contests/pp-term/leaderboard"
+contest_link = "https://www.hackerrank.com/contests/pp-term/leaderboard/"
 
-def call_link(link):
+def retrieve_leaderboard_info(driver, link):
     driver.get(link)
 
     while True:
@@ -24,54 +21,56 @@ def call_link(link):
             break
         driver.get(link)
 
-    divs = driver.find_elements(By.XPATH, "//div [contains(@class, 'leaderboard-list-view')]")
+    return driver.find_elements(By.XPATH, "//div [contains(@class, 'leaderboard-list-view')]")
 
-    for d in divs:
-        html_doc = d.get_attribute('innerHTML')
-        soup = BeautifulSoup(html_doc, 'html.parser')
-        key = soup.find('div', 'span-flex-2').p.text
-        value = len(soup.find_all('div', 'correct'))
+def retrieve_statistics(div):
+    html_doc = div.get_attribute('innerHTML')
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    key = soup.find('div', 'span-flex-2').p.text
+    value = len(soup.find_all('div', 'correct'))
+    return key, value
+
+def get_leaderboard_data(driver, link, additional=False):
+    divs = retrieve_leaderboard_info(driver, link)
+
+    for div in divs:
+        key, value = retrieve_statistics(div)
         
+        f, s = 0, 0
         if key in students:
             f, s = students[key]
-            students[key] = (f + value, s)
+
+        if additional:
+            s += value
         else:
-            students[key] = (value, 0) 
+            f += value
 
-def call_additional_contest(link):
-    driver.get(link)
+        students[key] = (f, s)
 
-    while True:
-        tmp_divs = driver.find_elements(By.XPATH, "//div[contains(@class, 'span-flex-2')]")
-        if len(tmp_divs) > 0:
-            break
-        driver.get(link)
+def print_html_table():
+    print("document.write('\\")
+    for student in sorted(students.items(), key=lambda x: (x[1][0], x[1][1], x[0]), reverse=True):
+        print("<tr>\\")
+        print("\t<td>" + str(student[0]) + "</td>\\")
+        print("\t<td>" + str(student[1][0]) + "</td>\\")
+        print("\t<td>" + str(student[1][1]) + "</td>\\")
+        print("</tr>\\a")
+    print("');")
 
-    divs = driver.find_elements(By.XPATH, "//div [contains(@class, 'leaderboard-list-view')]")
+def main():
+    driver = webdriver.Firefox()
 
-    for d in divs:
-        html_doc = d.get_attribute('innerHTML')
-        soup = BeautifulSoup(html_doc, 'html.parser')
-        key = soup.find('div', 'span-flex-2').p.text
-        value = len(soup.find_all('div', 'correct'))
-        
-        if key in students:
-            f, s = students[key]
-            students[key] = (f, s + value)
-        else:
-            students[key] = (0, value) 
+    for i in range(1, 6):
+        for j in range(1, 3):
+            link = contest + str(i) + leaderboard + str(j)
+            
+            get_leaderboard_data(driver, link)
 
-for i in range(1, 6):
     for j in range(1, 3):
-        link = contest + str(i) + leaderboard + str(j)
-        
-        call_link(link)
+        link = contest_link + str(j)
+        get_leaderboard_data(driver, link, True)
 
-call_additional_contest(contest_link)
+    print_html_table()
 
-for student in sorted(students.items(), key=lambda x: (x[1][0], x[1][1], x[0]), reverse=True):
-    print("<tr>")
-    print("\t<td>" + str(student[0]) + "</td>")
-    print("\t<td>" + str(student[1][0]) + "</td>")
-    print("\t<td>" + str(student[1][1]) + "</td>")
-    print("</tr>")
+if __name__ == "__main__":
+    main()
